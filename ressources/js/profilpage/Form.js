0ff724dc1816fb9e8471f37046211551dd4dd54d
Clément <lowriider23@gmail.com>
2021-09-08 13:35:37 +0200

    corrections de bugs

--- ressources/js/profilpage/Form.js
+++ ressources/js/profilpage/Form.js
@@ -16,53 +16,57 @@ export default class Form {
     // all var declared //
     this.regLetters = /^[a-zA-Zéèîï][a-zéèêàçîï]+([-'\s][a-zA-Zéèîï][a-zéèêàçîï]+)?/;
     this.regmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
-
+    this.message;
     this.firstNameOk = false;
     this.lastNameOk = false;
     this.emailOk = false;
     this.messageOk = false;
-    
+
     // EVENT STARTS WHEN YOU LEAVE INPUT FIELD
-    this.data.forEach(item => item.addEventListener('blur', function() {
-        // check first name field // 
-      if (item.id === "first") {
-        if (item.value.length < 2 || !this.regLetters.test(item.value)) { 
-          this.highlightField(item, true);
+    this.data.forEach(item => item.addEventListener('blur', function(e) {
+        // check first name field //
+      if (e.target.id === "first-name") {
+        if (e.target.value.length < 2 || !this.regLetters.test(e.target.value)) { 
+          this.highlightField(e.target, true);
         } else {
-          this.highlightField(item, false);
-          this.errorMessagesReset(item);
+          this.highlightField(e.target, false);
+          this.errorMessagesReset(e.target);
+          this.firstName = e.target.value;
           this.firstNameOk = true;
         }
           // check last name field // 
-      } else if (item.id === "last") {
-        if (item.value.length < 2 || !this.regLetters.test(item.value)) {
-          this.highlightField(item, true);
+      } else if (e.target.id === "last-name") {
+        if (e.target.value.length < 2 || !this.regLetters.test(e.target.value)) {
+          this.highlightField(e.target, true);
         } else {
-          this.highlightField(item, false);
-          this.errorMessagesReset(item);
+          this.highlightField(e.target, false);
+          this.errorMessagesReset(e.target);
+          this.lastName = e.target.value;
           this.lastNameOk = true;
         }
           // check email field // 
-      } else if (item.id === "email") {
-        if (item.value.length < 2 || !this.regmail.test(item.value)) {
-          this.highlightField(item, true);
+      } else if (e.target.id === "email") {
+        if (e.target.value.length < 2 || !this.regmail.test(e.target.value)) {
+          this.highlightField(e.target, true);
         } else {
-          this.highlightField(item, false);
-          this.errorMessagesReset(item);
+          this.highlightField(e.target, false);
+          this.errorMessagesReset(e.target);
+          this.email = e.target.value;
           this.emailOk = true;
         }
           // check textarea field // 
-      } else if (item.id === "message") {
-        if (item.value.length < 1 || item.value > 100) { // if length of item is sup or equal to 1 and 
-          this.highlightField(item, true);
+      } else if (e.target.id === "message") {
+        if (e.target.value.length < 1 || e.target.value > 100) { // if length of e.target is sup or equal to 1 and 
+          this.highlightField(e.target, true);
         } else {
-          this.highlightField(item, false);
-          this.errorMessagesReset(item);
+          this.highlightField(e.target, false);
+          this.errorMessagesReset(e.target);
+          this.message = e.target.value;
           this.messageOk = true;
         }
       }
     }.bind(this)));
-
+ 
     this.contact.addEventListener('click', function() {
       this.contact.style.display = "none";
       this.form.style.display = "block";
@@ -76,11 +80,14 @@ export default class Form {
 
     // submit form + message //
     this.submitFormOk.addEventListener('click', function(e) {
-
       e.preventDefault(); // prevent default action of the button if the form is not filled with all the infos //
       this.checkAllFields();
-
+      console.log(this.checkAllFields())
       if (this.checkAllFields()) {
+        console.log(this.firstName);
+        console.log(this.lastName);
+        console.log(this.email);
+        console.log(this.message);
         this.formOK();
         this.closeModal();
         this.reset();
@@ -143,38 +150,6 @@ export default class Form {
     } else
       field.style.backgroundColor = "";
     // errorMessagesReset(field);
-  }
-
-  // check last name field //
-  checkLast(field) {
-    if (field.value.length < 2 || !this.regLetters.test(field.value)) {
-      highlightField(field, true);
-    } else {
-      highlightField(field, false);
-      errorMessagesReset(field);
-      lastNameOk = true;
-    }
-  }
-  // check email //
-  checkMail(field) {
-    if (field.value.length < 2 || !this.regmail.test(field.value)) {
-      highlightField(field, true);
-    } else {
-      highlightField(field, false);
-      errorMessagesReset(field);
-      emailOk = true;
-    }
-  }
-
-  // check le champ message //
-  checkMessage(field) {
-    if (field.value.length < 1 || field.value > 100 || !this.regNumbers.test(field.value)) { // if length of field is sup or equal to 1 and 
-      highlightField(field, true);
-    } else {
-      highlightField(field, false);
-      errorMessagesReset(field);
-      messageOk = true;
-    }
   }
 
   checkAllFields() {
